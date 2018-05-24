602cbf177ec3f2f3da6f335d6d05bb86881171de
FRCB <france.bushman@gmail.com>
2018-05-23 19:51:54 -0600

    almost done

--- src/App.js
+++ src/App.js
@@ -1,49 +1,70 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import EventCard from './EventCard'
 
 class App extends Component {
   constructor() {
     super()
 
     this.state = {
-      events: [{ id: 1, name: 'John', event: 'Rock Skipping', desc: "We're going to skip some rocks on water and it's going to be oh so fun."},
-                   { id: 2, name: 'Alexis', event: 'Chip Dipping', desc: "We're going to dip so many chips in so many dips. I may even bring cheese. The good kind."},
-                   { id: 3, name: 'Riley', event: 'Speed Walking', desc: "We will be walking at insane speeds, but never having both feet off the ground at the same time."}],
+      events: [{ id: 1, name: 'John', title: 'Rock Skipping', desc: "We're going to skip some rocks on water and it's going to be oh so fun." },
+      { id: 2, name: 'Alexis', title: 'Chip Dipping', desc: "We're going to dip so many chips in so many dips. I may even bring cheese. The good kind." },
+      { id: 3, name: 'Riley', title: 'Speed Walking', desc: "We will be walking at insane speeds, but never having both feet off the ground at the same time." }],
       nameInput: '',
       titleInput: '',
-      descInput: '',
+      descInput: ''
     }
 
     this.id = 4
+    this.editEvent = this.editEvent.bind(this)
   }
 
   createEvent() {
-    let post = {id: this.id, name: this.state.nameInput, title: this.state.titleInput, desc: this.state.descInput}
-    this.setState({events: [...this.state.events, post]})
+    let post = { id: this.id, name: this.state.nameInput, title: this.state.titleInput, desc: this.state.descInput }
+    this.setState({ events: [...this.state.events, post] })
     this.id++
   }
 
+  editEvent(id, name, title, description) {
+    let eventsCopy = this.state.events.slice()
+    for (let i = 0; i < eventsCopy.length; i++) {
+      if (eventsCopy[i].id === id) {
+        eventsCopy[i].name = name
+        eventsCopy[i].title = title
+        eventsCopy[i].description = description
+      }
+    }
+    this.setState({ events: eventsCopy })
+  }
+
   render() {
+    let mapped = this.state.events.map((event, i) => {
+      return <EventCard
+        key={i + event.name}
+        event={event}
+        editEvent={this.editEvent}
+      />
+    })
     return (
-      <div className="App">
+      <div className="App" >
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Mapping Components</h1>
         </header>
         <h2>Create Event</h2>
         <h3>Name</h3>
-        <input onChange={e => this.setState({nameInput: e.target.value})}/>
+        <input onChange={e => this.setState({ nameInput: e.target.value })} />
         <h3>Title</h3>
-        <input onChange={e => this.setState({titleInput: e.target.value})}/>
+        <input onChange={e => this.setState({ titleInput: e.target.value })} />
         <h3 id='desc'>Description</h3>
-        <textarea onChange={e => this.setState({descInput: e.target.value})}/>
-        <br/><br/>
+        <textarea onChange={e => this.setState({ descInput: e.target.value })} />
+        <br /> <br />
         <button onClick={() => this.createEvent()}>Create Event</button>
         <hr />
         <h2>Events :D</h2>
         <div className='eventsDisplay'>
-          {/* Let's display our mapped components here */}
+          {mapped}
         </div>
       </div>
     );
---
+++ src/EventCard.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+export default class eventCard extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            editToggle: false,
+            editName: '',
+            editTitle: '',
+            editDesc: ''
+        }
+    }
+
+
+    componentDidMount() {
+        this.setState({
+            editName: this.props.event.name,
+            editTitle: this.props.event.title,
+            editDesc: this.props.event.desc
+        })
+    }
+
+    saveClick() {
+        if (this.state.editToggle) {
+            this.props.editEvent(this.props.event.id, this.state.editName, this.state.editTitle, this.state.editDesc)
+        }
+        this.setState({ editToggle: !this.state.editToggle })
+    }
+
+    render() {
+        console.log(this.state)
+        return (
+            <div
+                className='eventCard'>
+                {
+                    this.state.editToggle
+                        ?
+                        <div>
+                            <input
+                                value={this.state.editName}
+                                onChange={e => this.setState({ editName: e.target.value })} />
+                            <input
+                                value={this.state.editTitle}
+                                onChange={e => this.setState({ editTitle: e.target.value })} />
+                            <textarea
+                                value={this.state.editDesc}
+                                onChange={e => this.setState({ editDesc: e.target.value })} />
+                        </div>
+                        :
+                        <div>
+                            <h4>{this.props.event.name}</h4>
+                            <h4>{this.props.event.title}</h4>
+                            <p>{this.props.event.desc}</p>
+                        </div>
+                }
+
+                <button
+                    onClick={() => this.saveClick()}>
+                    {this.state.editToggle ? 'Save' : 'Edit'}
+                </button>
+                <button>Delete</button>
+            </div >
+        )
+    }
+}
